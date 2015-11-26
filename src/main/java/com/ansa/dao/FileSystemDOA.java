680f08a0895bda76456d5f6d8ea9b5472f4cb81e
Andrey Salkov <salkov.andrey@gmail.com>
2015-11-26 03:55:14 +0300

    image processing

---
+++ src/main/java/com/ansa/dao/FileSystemDOA.java
@@ -0,0 +1,7 @@
+package com.ansa.dao;
+
+/**
+ * Created by andrey on 26.11.2015.
+ */
+public class FileSystemDOA {
+}
---
+++ src/main/java/com/ansa/rest/ImageMetadata.java
@@ -0,0 +1,7 @@
+package com.ansa.rest;
+
+/**
+ * Created by andrey on 26.11.2015.
+ */
+public class ImageMetadata {
+}
